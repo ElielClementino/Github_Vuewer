0164c93a295a78ae304472d2392618b9579b8496
Eliel Clementino da Silva <elielclementinodasilva@gmail.com>
2023-01-19 20:03:16 -0300

    add debounce from loadash

--- src/components/GithubRepo.vue
+++ src/components/GithubRepo.vue
@@ -1,57 +1,46 @@
 <template>
-    <div>
-       <v-autocomplete
+    <v-app>
+        <v-autocomplete
         v-model="user"
         :items="userList"
         :loading="loading"
         :search-input.sync="search"
         />
-    </div>
+    </v-app>
 </template>
 
 <script>
-
+import debounce from 'lodash/debounce'
 export default {
     name: 'GithubRepo',
-    data: () => ({
-        user: null,
-        userList: [],
-        loading: false,
-        search: null,
-        debounce: null
-    }),
-    methods: {
-        debounceSearch () {
-            if (this.debounce) {
-                clearTimeout(this.debounce)
-            }
-            this.debounce = setTimeout(() =>{
-                this.getUsers()
-                this.debounce = null
-            }, 1000)
-        },
-        getUsers () {
-            this.loading = true
-            setTimeout(() => {
-                return new Promise((resolve, reject) => {
-                    fetch(`https://api.github.com/search/users?q=${this.search}`).then((response) =>{
-                        return response.json()
-                    }).then((data) => {
-                        this.userList = data.items.map((idx) => [idx.login])
-                        return resolve(data)
-                    }).catch((error) => {
-                        return reject(error)
-                    })
-                    this.loading = false
-                })
-            }, 2000)
-            
+    data () {
+        return {
+            user: null,
+            userList: {},
+            loading: false,
+            search: null
         }
     },
+    methods: {
+        getUsers () {
+            this.loading = true
+            return new Promise((resolve, reject) => {
+                fetch(`https://api.github.com/search/users?q=${this.search}`).then((response) =>{
+                    return response.json()
+                }).then((data) => {
+                    this.userList = data.items.map((idx) => [idx.login])
+                    return resolve(data)
+                }).catch((error) => {
+                    return reject(error)
+                })
+                this.loading = false
+            })
+        },
+    },
     watch: {
-        search () {
-            this.debounceSearch()
-        }
+        search: debounce(function () {
+            this.getUsers()
+        }, 2000)
     }
 }
 </script>
