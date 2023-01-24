b8478a86a28a7b9381e6886d514fbc671047a514
Eliel Clementino da Silva <elielclementinodasilva@gmail.com>
2023-01-24 00:55:40 -0300

    add v-card to choose which repo to get info about

--- src/components/GithubRepo.vue
+++ src/components/GithubRepo.vue
@@ -1,12 +1,51 @@
 <template>
-    <v-app>
+<v-container>
         <v-autocomplete
         v-model="user"
         :items="userList"
-        :loading="loading"
+        :loading="usersLoading"
+        item-text="login"
         :search-input.sync="search"
         />
-    </v-app>
+    <div
+    v-if="userInfo?.avatar_url"
+    > 
+        <v-card
+        :loading="userLoading"
+        class="mx-auto my-12"
+        max-width="374"
+        >
+            <template slot="progress">
+            <v-progress-linear
+                color="deep-purple"
+                height="10"
+                indeterminate
+            ></v-progress-linear>
+            </template>
+
+            <v-img
+            height="250"
+            :src="userInfo?.avatar_url"
+            ></v-img>
+            <v-card-title>{{ userInfo.login }}</v-card-title>
+            <v-card-text>
+                <v-card-title>Escolha um repositorio para ver mais informações</v-card-title>
+            <v-chip-group
+                v-model="selection"
+                active-class="deep-purple accent-4 white--text"
+                column
+            >
+                <v-chip 
+                v-for="chip in reposList"
+                :key=chip.id
+                >
+                    {{ chip?.full_name }}
+                </v-chip>
+            </v-chip-group>
+        </v-card-text>
+        </v-card>
+    </div>
+</v-container>
 </template>
 
 <script>
@@ -17,24 +56,66 @@ export default {
     data () {
         return {
             user: null,
+            repo: null,
             userList: [],
-            loading: false,
-            search: null
+            userInfo: {},
+            repoInfo: {},
+            repoInfoLoading: false,
+            userLoading: false,
+            usersLoading: false,
+            repoLoading: false,
+            search: null,
+            reposList: [],
+            selection: null
         }
     },
     methods: {
-        getUsers () {
-            this.loading = true
-            api.getUser(this.search).then((data) => {
-                this.userList = data.items.map((idx) => [idx.login])
+        async getUsers () {
+            this.usersLoading = true
+            await api.getUsers(this.search).then((data) => {
+                this.userList = data.items
             })
-            this.loading = false
+            this.usersLoading = false
         },
+        async getUser () {
+            this.userLoading = true
+            await api.getUser(this.user).then((data) => {
+                this.userInfo = data
+            })
+            this.userLoading = false
+        },
+        async getRepoInfo () {
+            this.repoInfoLoading = true
+            await api.getRepoInfo(this.reposList[this.selection]?.full_name).then((data) => {
+                this.repoInfo = data
+            })
+            this.repoInfoLoading = false
+        },
+        async getRepos () {
+            this.repoLoading = true
+            await api.getRepos(this.user).then((data) => {
+                this.reposList = data
+            })
+            this.repoLoading = false
+        }
     },
     watch: {
         search: debounce(function () {
-            this.getUsers()
-        }, 2000)
+            if (this.search != null) {
+                this.getUsers()
+            }
+        }, 2000),
+        user () {
+            if (this.user) {
+                this.getUser()
+                this.getRepos()
+            }
+        },
+        selection () {
+            if (this.selection) {
+                this.getRepoInfo()
+            }
+        }
     }
 }
 </script>
