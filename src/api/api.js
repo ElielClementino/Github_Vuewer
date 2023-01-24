f88263e45dbd8ab14cbc4d516cd1f12c7483135a
Eliel Clementino da Silva <elielclementinodasilva@gmail.com>
2023-01-24 00:53:15 -0300

    add method get to search for users and repos

--- src/api/api.js
+++ src/api/api.js
@@ -1,14 +1,32 @@
-import axios from "axios"
-
 export default {
-    async getUser (user) {
-        try {
-            const result = await axios.get(`https://api.github.com/search/users?q=${user}`).then((response) => {
-                return response.json()
-            })
-            return result
-        } catch (error){
-            console.error(error)
+    async getUsers (user, page) {
+        if (!page) {
+            page = 1
         }
+        const result = await fetch(`https://api.github.com/search/users?q=${user}&page${page}&per_page=100`).then((response) => {
+            return response.json()
+        })
+        return result
+    },
+    async getUser (user) {
+        const result = await fetch(`https://api.github.com/users/${user}`).then((response) => {
+            return response.json()
+        })
+        return result
+    },
+    async getRepoInfo (full_name) {
+        const result = await fetch(`https://api.github.com/repos/${full_name}`).then((response) => {
+            return response.json()
+        })
+        return result
+    },
+    async getRepos (user, page) {
+        if (!page) {
+            page = 1
+        }
+        const result = await fetch(`https://api.github.com/users/${user}/repos?page=${page}&per_page=100`).then((response) => {
+            return response.json()
+        })
+        return result
     }
 }
