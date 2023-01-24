4f629a74186f2325901d08c8da8c41795aad9681
Eliel Clementino da Silva <elielclementinodasilva@gmail.com>
2023-01-24 20:17:04 -0300

    Add card component to render folders and user info

--- src/components/GithubRepo.vue
+++ src/components/GithubRepo.vue
@@ -1,49 +1,122 @@
 <template>
 <v-container>
-        <v-autocomplete
-        v-model="user"
-        :items="userList"
-        :loading="usersLoading"
-        item-text="login"
-        :search-input.sync="search"
-        />
+    <v-row class="text-center">
+        <v-col cols="12">
+            <v-autocomplete
+            v-model="user"
+            :items="userList"
+            :loading="usersLoading"
+            item-text="login"
+            :search-input.sync="search"
+            />
+        </v-col>
+    </v-row>
     <div
     v-if="userInfo?.avatar_url"
     > 
-        <v-card
-        :loading="userLoading"
-        class="mx-auto my-12"
-        max-width="374"
-        >
-            <template slot="progress">
-            <v-progress-linear
-                color="deep-purple"
-                height="10"
-                indeterminate
-            ></v-progress-linear>
-            </template>
-
-            <v-img
-            height="250"
-            :src="userInfo?.avatar_url"
-            ></v-img>
-            <v-card-title>{{ userInfo.login }}</v-card-title>
-            <v-card-text>
-                <v-card-title>Escolha um repositorio para ver mais informações</v-card-title>
-            <v-chip-group
-                v-model="selection"
-                active-class="deep-purple accent-4 white--text"
-                column
+    <v-row dense align="stretch">
+        <v-col cols="6">
+            <v-card
+            :loading="userLoading"
+            class="mx-auto my-12"
+            max-width="374"
             >
-                <v-chip 
-                v-for="chip in reposList"
-                :key=chip.id
-                >
-                    {{ chip?.full_name }}
-                </v-chip>
-            </v-chip-group>
-        </v-card-text>
-        </v-card>
+                <template slot="progress">
+                <v-progress-linear
+                    color="deep-purple"
+                    height="10"
+                    indeterminate
+                ></v-progress-linear>
+                </template>
+
+                <v-img
+                height="250"
+                :src="userInfo?.avatar_url"
+                ></v-img>
+                <v-card-title>{{ userInfo?.login }}</v-card-title>
+                <v-card-text>
+                    <v-card-title>Escolha um repositorio para ver mais informações</v-card-title>
+                    <v-chip-group
+                        v-model="selection"
+                        active-class="deep-purple accent-4 white--text"
+                        column
+                    >
+                        <v-chip 
+                        v-for="repo in reposList"
+                        :key=repo.id
+                        >
+                            <v-icon>mdi-folder</v-icon> {{ repo?.full_name }}
+                        </v-chip>
+                    </v-chip-group>
+                </v-card-text>
+            </v-card>
+        </v-col>
+        <v-col cols="6"
+        v-if="selection"
+        >
+            <v-card
+            :loading="foldersLoading"
+            class="mx-auto my-12"
+            max-width="374"
+            >
+                <template slot="progress">
+                    <v-progress-linear
+                        color="deep-purple"
+                        height="10"
+                        indeterminate
+                    ></v-progress-linear>
+                </template>
+                <v-card-text>
+                    <v-card-title>Escolha um</v-card-title>
+                    <v-chip-group
+                        v-model="folderSelection"
+                        active-class="deep-purple accent-4 white--text"
+                        column
+                    >
+                        <v-chip 
+                        v-for="folder in folders"
+                        :key=folder.id
+                        >
+                            <v-icon v-if="folder?.type === 'dir' ">mdi-folder</v-icon>
+                            <v-icon v-if="folder?.type === 'file' ">mdi-file</v-icon> 
+                            {{ folder?.name }}
+                        </v-chip>
+                    </v-chip-group>
+                </v-card-text>
+            </v-card>
+            <v-card
+            v-if="folderSelection"
+            :loading="foldersLoading"
+            class="mx-auto my-12"
+            max-width="374"
+            >
+                <template slot="progress">
+                    <v-progress-linear
+                        color="deep-purple"
+                        height="10"
+                        indeterminate
+                    ></v-progress-linear>
+                </template>
+                <v-card-text>
+                    <v-chip-group
+                        v-model="chosenFolderPath"
+                        active-class="deep-purple accent-4 white--text"
+                        column
+                    >
+                        <v-chip 
+                        v-for="folder in  selectedFolder "
+                        :key=folder.id
+                        >
+                            <v-icon v-if="folder?.type === 'dir' ">mdi-folder</v-icon>
+                            <v-icon v-if="folder?.type === 'file' ">mdi-file</v-icon> 
+                            {{ folder?.name }}
+                        </v-chip>
+                    </v-chip-group>
+                    <v-card-title>Caminho atual:{{ selectedFolder[chosenFolderPath]?.path }}</v-card-title>
+                </v-card-text>
+            </v-card>
+        </v-col>
+    </v-row>
     </div>
 </v-container>
 </template>
@@ -57,16 +130,21 @@ export default {
         return {
             user: null,
             repo: null,
+            chosenFolderPath: null,
             userList: [],
             userInfo: {},
             repoInfo: {},
+            folders: [],
+            selectedFolder: {},
             repoInfoLoading: false,
+            foldersLoading: false,
             userLoading: false,
             usersLoading: false,
             repoLoading: false,
             search: null,
             reposList: [],
-            selection: null
+            selection: null,
+            folderSelection: null
         }
     },
     methods: {
@@ -97,6 +175,20 @@ export default {
                 this.reposList = data
             })
             this.repoLoading = false
+        },
+        async getFolders () {
+            this.foldersLoading = true
+            await api.getFolders(this.reposList[this.selection]?.full_name).then((data) => {
+                this.folders = data
+            })
+            this.foldersLoading = false
+        },
+        async getSelectedFolder (path) {
+            this.foldersLoading = true
+            await api.getFolders(this.reposList[this.selection]?.full_name, path).then((data) => {
+                this.selectedFolder = data
+            })
+            this.foldersLoading = false
         }
     },
     watch: {
@@ -114,6 +206,21 @@ export default {
         selection () {
             if (this.selection) {
                 this.getRepoInfo()
+            }
+        },
+        repoInfo () {
+            if (this.repoInfo) {
+                this.getFolders()
+            }
+        },
+        folderSelection () {
+            if (this.folderSelection) {
+                this.getSelectedFolder(this.folders[this.folderSelection]?.path)
+            }
+        },
+        chosenFolderPath () {
+            if (this.chosenFolderPath && this.selectedFolder[this.chosenFolderPath]?.type === "dir") {
+                this.getSelectedFolder(this.selectedFolder[this.chosenFolderPath]?.path)
             }
         }
     }
